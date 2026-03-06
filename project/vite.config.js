65b4ad3e50a6a1555bbc7a981540e7e64650257b
Assignment-1 <sayoidrasel80@gmail.com>
2026-03-06 18:43:05 +0600

    Check

--- project/vite.config.js
+++ project/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "Restart-Assignment-2git rm --cached Mission-Restart-A2"
-})
+  base: "Restart-Assignment-2",
+});
