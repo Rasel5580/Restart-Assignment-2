497e0c91d7be8e921fb200ab392f5e44b68de151
Assignment-1 <sayoidrasel80@gmail.com>
2026-03-06 18:37:02 +0600

    Remove broken submodule

--- project/vite.config.js
+++ project/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "Restart-Assignment-2"
+  base: "Restart-Assignment-2git rm --cached Mission-Restart-A2"
 })
